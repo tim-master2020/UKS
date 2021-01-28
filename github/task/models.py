df7e3e3a9b9e5a4f749f671a0a82599b8b0820aa
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2021-01-28 20:18:06 +0100

    created task model

--- github/task/models.py
+++ github/task/models.py
@@ -21,3 +21,17 @@ class TaskType(Enum):
     BUG_FIX = "BUG_FIX"
     FEATURE = "FEATURE"
 
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    openingDate = models.DateTimeField(auto_now_add=True)
+    closingDate = models.DateTimeField(null=True)
+    taskStatus = models.CharField(max_length=50,choices = [(tag, tag.value) for tag in TaskStatus], default="TO_DO")
+    taskState = models.CharField(max_length=50,choices = [(tag, tag.value) for tag in TaskState],default="OPENED")
+    taskPriorty = models.CharField(max_length=50,choices = [(tag, tag.value) for tag in TaskPriorty],default="LOW")
+    taskType = models.CharField(max_length=50,choices = [(tag, tag.value) for tag in TaskType],default="FEATURE")
+    label = models.ManyToManyField(Label)
+
+
+
+
