eb1e57523a72c771bf1d5e6603519a34d09100f5
OljaSekulic <olja.sekulic.77@gmail.com>
2021-01-29 15:16:54 +0100

    Added relation between user and commit

--- github/commit/models.py
+++ github/commit/models.py
@@ -1,7 +1,9 @@
-from django.db import models
+from django.db import models 
+from user.models import UserModel
 
 class Commit(models.Model):
     sha = models.CharField(max_length=100)
     message = models.CharField(max_length=200)
     size = models.IntegerField()
     commitedDate = models.DateTimeField(auto_now_add=True)
+    user=models.ForeignKey(UserModel, verbose_name='user', on_delete=models.CASCADE)
