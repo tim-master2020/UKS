a82e14a4faa94de51613db3eafd77c90715ce769
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2021-01-29 17:24:58 +0100

    updated comment model
    added user field

--- github/comment/models.py
+++ github/comment/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from task.models import Task
+from user.models import UserModel
 
 class Comment(models.Model):
     text = models.CharField(max_length=300)
     date = models.DateTimeField(auto_now_add=True)
     file = models.FileField(null=True, blank=True)
     task = models.ForeignKey(to=Task, on_delete=models.CASCADE,null=True)
+    user = models.ForeignKey(UserModel, verbose_name='author', on_delete=models.CASCADE)
