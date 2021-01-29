3101316d6e17cecc54dbaf6b80b9ef3e5d79ed16
tjncc <55355686+tjncc@users.noreply.github.com>
2021-01-29 19:07:47 +0100

    Added relationship between Project and Repository models.

--- github/project/models.py
+++ github/project/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from wiki.models import Wiki
+from repository.models import Repository
 
 # Create your models here.
 
@@ -7,3 +8,4 @@ class Project(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=300)
     wiki = models.OneToOneField(Wiki, on_delete=models.CASCADE)
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
