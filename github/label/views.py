7ce3cf9f4151a04c3411507a47fa4926619d3519
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2021-01-31 15:54:38 +0100

    created view for rendering all labels

--- github/label/views.py
+++ github/label/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import loader
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Label
+
+# Create your views here.
+
+def all(request):
+    labels = Label.objects.all
+    context = {'all_labels': labels}
+    return render(request, 'label/all-labels.html',context)
