f3be11dd021fc9d544c70225081f68b4fbb4c377
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2021-01-31 16:54:31 +0100

    added label repository relationship
    added repo field in label model

--- github/label/models.py
+++ github/label/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.core.validators import RegexValidator
-
+from repository.models import Repository;
 class Label(models.Model):
     name = models.CharField(max_length=100,unique=True)
+    repo = models.ForeignKey(to=Repository,verbose_name='repo', on_delete=models.CASCADE)
     color = models.CharField(max_length=7, validators=[RegexValidator(regex='^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$', message="Color must have a hex value")])
 
     def __str__(self):
