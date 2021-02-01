83f14114d5538f31d4f44220387eeede230438c3
minamaras97 <minamaras@outlook.com>
2021-02-01 21:53:33 +0100

    Fix pull request comments

--- github/repository/forms.py
+++ github/repository/forms.py
@@ -1,16 +1,11 @@
 from django import forms 
 from repository.models import Repository 
   
-  
-# creating a form 
 class RepositoryForm(forms.ModelForm): 
   
-    # create meta class 
     class Meta: 
-        # specify model to be used 
         model = Repository 
-  
-        # specify fields to be used 
+   
         fields = [ 
             "name" 
         ] 
--- github/repository/views.py
+++ github/repository/views.py
@@ -33,52 +33,36 @@ def addRepository(request):
     return render(request, 'repository/addRepository.html', {'form': form})
 
 def delete_view(request, id): 
-    # dictionary for initial data with  
-    # field names as keys 
     context ={} 
   
-    # fetch the object related to passed id 
     obj = get_object_or_404(Repository, id = id) 
   
   
     if request.method =="POST": 
-        #delete object 
-        obj.delete() 
-        #after deleting redirect to  
-        #home page 
+ 
+        obj.delete()
         return HttpResponseRedirect("/repository") 
   
     return HttpResponseRedirect("/repository")
 
-# update view for details 
-def update_view(request, id): 
-    # dictionary for initial data with  
-    # field names as keys 
+def update_view(request, id):  
     context ={} 
-  
-    # fetch the object related to passed id 
+   
     obj = get_object_or_404(Repository, id = id) 
-  
-    # pass the object as instance in form 
+
     form = RepositoryForm(request.POST or None, instance = obj) 
   
-    # save the data from the form and 
-    # redirect to detail_view 
     if form.is_valid(): 
         form.save() 
         return HttpResponseRedirect("/repository") 
   
-    # add form dictionary to context 
     context["form"] = form 
   
     return render(request, "allRepositories.html", context)
 
 def detail_view(request, id): 
-    # dictionary for initial data with  
-    # field names as keys 
     context ={} 
-   
-    # add the dictionary during initialization 
+
     context["repoData"] = Repository.objects.get(id = id) 
            
     return render(request, "repository/detailRepository.html", context)
