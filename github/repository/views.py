fb1c450c7013aa17a2bcf4834f4b3011d069bb9e
minamaras97 <minamaras@outlook.com>
2021-02-01 21:37:26 +0100

    Create a method for rendering all repositories

--- github/repository/views.py
+++ github/repository/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import (get_object_or_404, 
+                              render,  
+                              HttpResponseRedirect,
+                              redirect) 
+from .models import Repository
+from  branch.models import Branch
+from django.contrib import messages
+from .forms import RepositoryForm
+from .forms import RepositoryForm
 
-# Create your views here.
+
+def allRepositories(request):
+    allRepositories = Repository.objects.all()
+    #treba pristupiti brancount polju iz svakog repozitorijuma i staviti mu vrednost iz baze i takve objekte sacuvati u allRepositories
+    branchCount = Branch.objects.all()
+    context = {'allRepositories': allRepositories, 'branchCount' : branchCount}
+    return render(request, "repository/allRepositories.html", context)
