2caf4f3ae902d413b0702801708d6a9b6827e3b0
minamaras97 <minamaras@outlook.com>
2021-02-01 21:35:41 +0100

    Add a path for update repository

--- github/repository/url.py
+++ github/repository/url.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.allRepositories, name='allRepositories'),
     path('add', views.addRepository, name='addRepository'),
     path('<id>/delete', views.delete_view, name='deleteRepository'),
+    path('<id>/update', views.update_view, name='updateRepository'),
 ]
