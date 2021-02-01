4b7516e6389bc2a026c49b6dac9a1e3fec9569ea
OljaSekulic <olja.sekulic.77@gmail.com>
2021-02-01 14:15:08 +0100

    Changed user form

--- github/user/models.py
+++ github/user/models.py
@@ -5,7 +5,8 @@ from django.core.validators import RegexValidator
 
 from django.contrib.auth.models import User
 from django.utils import timezone
-
+from django import forms
+from django.core.validators import RegexValidator
 
 class UserProxy(User):
     class Meta:
@@ -13,23 +14,6 @@ class UserProxy(User):
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name + ' (' + self.email + ')'
-
-
-class UserModel(models.Model):
-    user = models.OneToOneField(UserProxy, related_name='profile', on_delete=models.PROTECT)
-    username=models.CharField('username', max_length=500)
-    name = models.CharField('name', max_length=500)
-    first_name=models.CharField('first name', max_length=500)
-    last_name=models.CharField('last name', max_length=500)
-    email = models.EmailField('email', max_length=500)
-    password = models.EmailField('password', max_length=500)
-
-    def __str__(self):
-        return self.username
-
-    class Meta:
-        verbose_name = 'GitHub user'
-        verbose_name_plural = 'GitHub users'
 
 class UserForm(forms.ModelForm):
     first_name = forms.CharField(max_length=30, required=True, help_text='Required.')
