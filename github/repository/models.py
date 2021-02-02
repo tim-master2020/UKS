9d090e0ff5e378044a2301501b9d1b7c2c552244
tjncc <55355686+tjncc@users.noreply.github.com>
2021-02-02 18:33:47 +0100

    Changed wiki's relationship.

--- github/repository/models.py
+++ github/repository/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from wiki.models import Wiki
 from django.utils.translation import gettext_lazy as _
 
 
 class Repository(models.Model):
     name = models.CharField(max_length=50)
     creationDate = models.DateTimeField(auto_now_add=True)
+    wiki = models.OneToOneField(Wiki, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
