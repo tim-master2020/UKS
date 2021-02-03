69462ae230c4d0e726ffb66045b18fccb5e319c0
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2021-02-03 18:04:05 +0100

    added needed imports for label view

--- github/label/views.py
+++ github/label/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import (get_object_or_404, 
+                              render,  
+                              HttpResponseRedirect,
+                              redirect)
+from django.urls import reverse
+from urllib.parse import urlencode
 from django.template import loader
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Label
+from .forms import LabelForm
+from repository.models import Repository
+from repository.views import detail_view
+from django.core.exceptions import ValidationError
 
 # Create your views here.
 
