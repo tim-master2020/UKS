2091f6dd946d2eefe6ed22282ba14d5fc0a5a2de
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2021-02-03 17:59:26 +0100

    added url for deleting existing label

--- github/label/urls.py
+++ github/label/urls.py
@@ -5,5 +5,7 @@ app_name='labels'
 
 urlpatterns = [
     path('all',views.all,name="all"),
-]    path('add/<id>',views.addLabel,name="addLabel"),
+    path('add/<id>',views.addLabel,name="addLabel"),
     path('update/<id>/<label_id>',views.updateLabel,name="updateLabel"),
+    path('delete/<id>/<label_id>',views.deleteLabel,name="deleteLabel"),
+]
