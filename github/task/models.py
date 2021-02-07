8ac39a015b987a47ec0d6877c73408fe92ddc57f
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2021-02-07 01:22:42 +0100

    updated field closingDate to not be required

--- github/task/models.py
+++ github/task/models.py
@@ -29,7 +29,6 @@ class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=300,blank=True)
     openingDate = models.DateTimeField(auto_now_add=True)
-    closingDate = models.DateTimeField(null=True)
     taskStatus = models.CharField(max_length=50,choices = [(tag, tag.value) for tag in TaskStatus], default="TO_DO")
     taskState = models.CharField(max_length=50,choices = [(tag, tag.value) for tag in TaskState],default="OPENED")
     taskPriorty = models.CharField(max_length=50,choices = [(tag, tag.value) for tag in TaskPriorty],default="LOW")
