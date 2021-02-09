364e8916a196a1b83c5c0f4fe3d9484838c963ec
OljaSekulic <57363975+OljaSekulic@users.noreply.github.com>
2021-02-09 23:59:08 +0100

    Add tasks to project (#65)
    * Added multiselect for tasks in project
    
    * added html detailProject for adding tasks
    * added multiselect for all tasks for selected project
    
    * Added multiselect for tasks in project details
    
    * added details button
    * added multiselect in detailProject.html
    * added method for getting task which is not in current project

--- github/project/urls.py
+++ github/project/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.all_projects, name='allProjects'),
     path('add/<id>', views.add_project, name='addProject'),
     path('update/<id>/<project_id>', views.update_project, name='updateProject'),
-    path('delete/<id>/<project_id>',views.delete_project, name='deleteProject')
+    path('delete/<id>/<project_id>',views.delete_project, name='deleteProject'),
+    path('addTasks/<project_id>',views.add_tasks, name='addTasks'),
+    path('<project_id>/detailProject', views.project_detail, name='detailProject')
 ]
--- github/project/views.py
+++ github/project/views.py
@@ -7,6 +7,9 @@ from .forms import ProjectForm
 from django.urls import reverse
 from  branch.models import Branch
 from django.contrib import messages
+from django.http import HttpResponse
+from task.models import Task
+from repository.models import Repository
 
 # Create your views here.
 
@@ -52,3 +55,32 @@ def delete_project(request, id, project_id):
         obj.delete()
   
     return redirect(reverse("repository:detailRepository",args=(id)))
+
+def add_tasks(request, project_id):
+    selectedTasks = request.POST.getlist("tasks")
+    obj = get_object_or_404(Project, id = project_id)
+    repo = Repository.objects.get(id = obj.repository_id)  
+    
+    for item in selectedTasks:
+        task = Task.objects.get(id = item)
+        task.project.add(obj)
+        task.save()
+    return redirect(reverse("project:detailProject",args=[obj.id]))
+
+def project_detail(request, project_id): 
+    context ={} 
+    newTasks = []
+    resTasks = []
+    tasks = Task.objects.all()
+    for task in Task.objects.all():
+        for project in task.project.all():
+            if str(project.id)==str(project_id):
+                newTasks.append(task)
+    for task in Task.objects.all():
+        if task not in newTasks:
+            resTasks.append(task)
+    context["newtasks"] = resTasks
+    context["projectData"] = Project.objects.get(id = project_id) 
+    return render(request, "project/detailProject.html", context)
+
+
