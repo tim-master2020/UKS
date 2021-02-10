c58a1d2d2bc9b3f122fcc03055c10934d41376f3
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2021-02-10 18:12:21 +0100

    update task detail view to return comment form for adding new comment

--- github/task/views.py
+++ github/task/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 from repository.models import Repository
 from label.models import Label
 from project.models import Project
+from comment.models import Comment
+from comment.forms import CommentForm
 
 from django.shortcuts import (get_object_or_404, 
                               render,  
@@ -12,7 +14,7 @@ from django.shortcuts import (get_object_or_404,
                               redirect)
 from django.urls import reverse
 
-def detailView(request, id): 
+def detailView(request, id,form = None): 
     if request.method == 'GET':
         print('in get')
         context ={} 
@@ -32,7 +34,10 @@ def detailView(request, id):
         'project':[project for project in task.project.values_list('id', flat=True)]},
         repo_id = repo_id,)
 
-        return render(request, "task/detail-task.html", {'form': form,'repo_id':repo_id,'task_id':id})
+        comments = Comment.objects.filter(task_id = id)
+        commentForm = CommentForm()
+
+        return render(request, "task/detail-task.html", {'form': form,'repo_id':repo_id,'task_id':id,'comments':comments,'commentForm':commentForm,'currentUser':request.user})
         
     if request.method == 'POST':
         obj = get_object_or_404(Task, id = id)
@@ -45,11 +50,14 @@ def detailView(request, id):
             print('not valid form when updating!',form.errors)
 
 def newTask(request,repo_id): 
-    if request.method == 'GET': 
+    print('in new task view');
+    if request.method == 'GET':
+        print('in new task view GET'); 
         form = TaskForm(repo_id = repo_id)
         return render(request, "task/detail-task.html", {'form': form,'repo_id':repo_id,'is_adding':True})
 
     if request.method == 'POST':
+        print('in new task view POST');
         print('request is',request)
         form = TaskForm(request.POST)
         if form.is_valid():
@@ -75,4 +83,6 @@ def deleteTask(request,id):
     if request.method =="POST": 
         obj.delete() 
     return redirect(reverse("repository:detailRepository",args=[repo.id]))
+
+
 
