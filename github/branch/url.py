70f4a774eb6942056d76e25f539d72e245d2a1da
minamaras97 <minamaras@outlook.com>
2021-02-12 23:31:35 +0100

    Add branch url file

---
+++ github/branch/url.py
@@ -0,0 +1,12 @@
+from . import views
+from django.urls import path
+
+
+app_name = 'branch'
+urlpatterns = [
+    path('', views.allBranches, name='allBranches'),
+    path('add/<id>', views.addBranch, name='addBranch'),
+    path('delete/<id>/<branch_id>', views.delete_view, name='deleteBranch'),
+    path('update/<id>/<branch_id>', views.update_view, name='updateBranch'),
+    path('<id>/detailBranch', views.detail_view, name='detailBranch')
+]
