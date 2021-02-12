fe842a954aff4a6109593f6c411b97233b517f9c
tjncc <55355686+tjncc@users.noreply.github.com>
2021-02-12 18:45:38 +0100

    Created photo entity.
    Created photo entity for adding user profile photo and implemented method for uploading photo.

---
+++ github/photo/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse 
+from django.shortcuts import render, redirect 
+from .forms import *
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+def addPhoto(request): 
+    print(request.user.id)
+    if request.method == 'POST': 
+        form = PhotoForm(request.POST, request.FILES) 
+        
+        if form.is_valid():
+            photo = form.save(commit=False)
+            currentUser = User.objects.get(id = request.user.id)
+            photo.users = currentUser
+            photo.save()
+            return redirect('/profile')
+    else: 
+        form = PhotoForm() 
+    return redirect('/profile') 
