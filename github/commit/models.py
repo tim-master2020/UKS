cc72a72e44499d30a21872200604b999dd1c783c
OljaSekulic <olja.sekulic.77@gmail.com>
2021-02-13 13:29:25 +0100

    Deleted size field from commit

--- github/commit/models.py
+++ github/commit/models.py
@@ -5,7 +5,6 @@ from branch.models import Branch
 class Commit(models.Model):
     sha = models.CharField(max_length=100)
     message = models.CharField(max_length=200)
-    size = models.IntegerField()
     commitedDate = models.DateTimeField(auto_now_add=True)
     user=models.ForeignKey(User, verbose_name='user', on_delete=models.CASCADE)
     branch = models.ForeignKey(to=Branch, on_delete=models.DO_NOTHING,null=True)
