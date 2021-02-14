60c59e2ff85e232d845a6b2119af62c21e72dfdd
minamaras97 <minamaras@outlook.com>
2021-02-14 11:21:29 +0100

    Update models.py

--- github/branch/models.py
+++ github/branch/models.py
@@ -5,6 +5,7 @@ from repository.models import Repository
 
 class Branch(models.Model):
     name = models.CharField(max_length=50)
+    baseBranch = models.CharField(blank=True, max_length=80)
     repository = models.ForeignKey(to=Repository, on_delete=models.CASCADE)
 
     def __str__(self):
