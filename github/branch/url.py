ab0668c424ca94445dd2afffde41bd9f6a0a5ab4
Mina Maras <40916170+minamaras@users.noreply.github.com>
2021-02-14 11:29:18 +0100

    Feature create branch from another (#77)
    * Update models.py
    
    * Update url.py
    
    * Create a method for rebasing a branch
    
    * Update allBranches.html
    
    * Create newBranch.html
    
    Co-authored-by: minamaras97 <minamaras@outlook.com>

--- github/branch/url.py
+++ github/branch/url.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('add/<id>', views.addBranch, name='addBranch'),
     path('delete/<id>/<branch_id>', views.delete_view, name='deleteBranch'),
     path('update/<id>/<branch_id>', views.update_view, name='updateBranch'),
-    path('<id>/detailBranch', views.detail_view, name='detailBranch')
+    path('<id>/detailBranch', views.detail_view, name='detailBranch'),
+    path('createABranchFromExisting/<id>', views.createABranchFromExisting, name='createABranchFromExisting'),
 ]
