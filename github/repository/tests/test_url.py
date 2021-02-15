d2599e201fe074725be62037b46fdf94457b4295
minamaras97 <minamaras@outlook.com>
2021-02-15 14:17:53 +0100

    Test allRepositories url

---
+++ github/repository/tests/test_url.py
@@ -0,0 +1,8 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from repository.views import allRepositories, addRepository, delete_view, update_view, detail_view
+
+class TestUrl(SimpleTestCase):
+    def test_all_url_is_resolved(self):
+        url = reverse('repository:allRepositories')
+        self.assertEquals(resolve(url).func, allRepositories)
