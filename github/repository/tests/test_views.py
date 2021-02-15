b74f41d69e6dc9a2a85f497701f621b377000b6d
Mina Maras <40916170+minamaras@users.noreply.github.com>
2021-02-15 14:33:45 +0100

    Test repository (#98)
    * Delete tests.py
    
    * Create __init__.py
    
    * Test is form valid with no members
    
    * Test is form valid with a member
    
    * test if form is not valid
    
    * Test allRepositories url
    
    * Test addRepository url
    
    * Test deleteRepository url
    
    * Test updateRepository url
    
    * Test detailRepository url
    
    * Add setUp method
    
    * Test allRepositories method
    
    * Test adding new repository with no members
    
    * Test adding new repository with a member
    
    * Test if master branch is created when adding a new repository
    
    * Test delete a repository
    
    * Test update repository
    
    Co-authored-by: minamaras97 <minamaras@outlook.com>

---
+++ github/repository/tests/test_views.py
@@ -0,0 +1,90 @@
+from django.http import response
+from django.test import TestCase, Client, client
+from django.urls import reverse, resolve
+from repository.views import allRepositories, addRepository, delete_view, update_view, detail_view
+from repository.models import Repository
+from branch.models import Branch
+from django.contrib.auth.models import User
+import json
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client();
+
+        self.allRepositoriesUrl = reverse('repository:allRepositories')
+        self.addRepository = reverse('repository:addRepository')
+        self.deleteRepository = reverse('repository:deleteRepository', args=[4])
+        self.deleteRepositoryNoId = reverse('repository:deleteRepository', args=[11])
+        self.updateRepository = reverse('repository:updateRepository', args=[5])
+
+        self.loggedInUser = User.objects.create_user(username='testuser', password='12345')
+        login = self.client.login(username='testuser', password='12345')
+
+    def test_allRepositories_GET(self):
+        response = self.client.get(self.allRepositoriesUrl)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'repository/allRepositories.html')
+    
+    def test_add_new_repository_no_members(self):
+        response = self.client.post(self.addRepository, {
+            'name' : 'UKS',
+            'user' : ['Choose members']
+        })
+
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(Repository.objects.get(name='UKS').name, 'UKS')
+
+    def test_add_new_repository_withMembers(self):
+        member = User.objects.create_user(username='testmember', password='12345')
+        response = self.client.post(self.addRepository, {
+            'name' : 'UKS1',
+            'user' : ['testmember']
+        })
+
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(Repository.objects.get(name='UKS1').name, 'UKS1')
+
+    def test_add_new_repository_check_is_master_branch_created(self):
+        response = self.client.post(self.addRepository, {
+            'name' : 'UKS2',
+            'user' : ['Choose members']
+        })
+
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(Repository.objects.get(name='UKS2').name, 'UKS2')
+        self.assertEquals(Branch.objects.get(name='master').name, 'master')
+
+    def test_delete_existing_repository(self):
+        response = self.client.post(self.addRepository, {
+            'name' : 'repo',
+            'user' : ['Choose members']
+        })
+
+        self.assertEquals(Repository.objects.count(), 1)
+
+        response = self.client.post(self.deleteRepository)
+
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(Repository.objects.count(), 0)
+    
+    def test_update_repository(self):
+        response = self.client.post(self.addRepository, {
+            'name' : 'repo',
+            'user' : ['Choose members']
+        })
+
+        self.assertEquals(Repository.objects.count(), 1)
+        self.assertEquals(Repository.objects.get(name='repo').name, 'repo')
+
+        response = self.client.post(self.updateRepository, {
+            'name' : 'new name'
+        })
+
+        self.assertEquals(Repository.objects.get(name='new name').name, 'new name')
+
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(Repository.objects.count(), 1)
+
+    
