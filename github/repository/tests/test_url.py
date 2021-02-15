b8ffd98afc8f9d1e52ed1235a392d5d9585081b0
minamaras97 <minamaras@outlook.com>
2021-02-15 14:18:17 +0100

    Test addRepository url

--- github/repository/tests/test_url.py
+++ github/repository/tests/test_url.py
@@ -6,3 +6,7 @@ class TestUrl(SimpleTestCase):
     def test_all_url_is_resolved(self):
         url = reverse('repository:allRepositories')
         self.assertEquals(resolve(url).func, allRepositories)
+
+    def test_add_url_is_resolved(self):
+        url = reverse('repository:addRepository')
+        self.assertEquals(resolve(url).func, addRepository)
