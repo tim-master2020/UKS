b084d12f49a3c81ca383b9af87a89ff7a049921d
minamaras97 <minamaras@outlook.com>
2021-02-15 14:20:47 +0100

    Test allRepositories method

--- github/repository/tests/test_views.py
+++ github/repository/tests/test_views.py
@@ -21,4 +21,11 @@ class TestViews(TestCase):
         self.loggedInUser = User.objects.create_user(username='testuser', password='12345')
         login = self.client.login(username='testuser', password='12345')
 
+    def test_allRepositories_GET(self):
+        response = self.client.get(self.allRepositoriesUrl)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'repository/allRepositories.html')
+    
+
     
