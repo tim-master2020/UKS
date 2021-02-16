ee0faf6939567472533815e6256f5263b9b11631
OljaSekulic <olja.sekulic.77@gmail.com>
2021-02-16 12:34:20 +0100

    Added form tests

--- github/user/tests/test_forms.py
+++ github/user/tests/test_forms.py
@@ -27,6 +27,38 @@ class TestForms(TestCase):
         form = UserForm(data={})
 
         self.assertFalse(form.is_valid())
+    
+    def test_user_without_first_name_is_not_valid(self):
+        form = UserForm(data={
+            'username' : 'Korisnik',
+            'last_name' : 'OvoJePrezime',
+            'password' : 'Sifra1234567!',
+            'email' : 'email@gmail.com'
+        })
+
+        self.assertFalse(form.is_valid())
+    
+    def test_user_with_invalid_password_is_not_valid(self):
+        form = UserForm(data={
+            'username' : 'Korisnik',
+            'first_name' : 'OvoJeIme',
+            'last_name' : 'OvoJePrezime',
+            'password' : 'Sifra!',
+            'email' : 'email@gmail.com'
+        })
+
+        self.assertFalse(form.is_valid())
+
+    def test_user_with_invalid_email_is_not_valid(self):
+        form = UserForm(data={
+            'username' : 'Korisnik',
+            'first_name' : 'OvoJeIme',
+            'last_name' : 'OvoJePrezime',
+            'password' : 'Sifra1234567!',
+            'email' : 'emailgmail.com'
+        })
+
+        self.assertFalse(form.is_valid())
 
     def test_edit_form_is_not_valid(self):
         form = UserUpdateForm(data={})
