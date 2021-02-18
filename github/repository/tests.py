6be7148f7315163639f46d01850391ccf5c6836d
minamaras97 <minamaras@outlook.com>
2021-02-18 14:17:21 +0100

    Update

--- github/repository/tests.py
+++ github/repository/tests.py
@@ -1,11 +1,58 @@
-from django.http import response
-from django.test import TestCase, Client, client
+from django.test import TestCase
+from repository.forms import RepositoryForm
+from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from repository.views import allRepositories, addRepository, delete_view, update_view, detail_view
 from repository.models import Repository
 from branch.models import Branch
 from django.contrib.auth.models import User
 import json
+from django.http import response
+from django.test import Client, client
+from django.urls import reverse, resolve
+
+class TestForms(TestCase):
+    def test_repository_form_is_valid_no_members(self):
+        form = RepositoryForm(data={
+            'name' : 'Repo',
+            'user' : ['Choose members']
+        })
+
+        self.assertTrue(form.is_valid())
+
+    def test_repository_form_is_valid_with_members(self):
+        form = RepositoryForm(data={
+            'name' : 'Repo',
+            'user' : ['mina']
+        })
+
+        self.assertTrue(form.is_valid())
+
+    def test_repository_form_is_not_valid(self):
+        form = RepositoryForm(data={})
+
+        self.assertFalse(form.is_valid())
+
+class TestUrl(SimpleTestCase):
+    def test_all_url_is_resolved(self):
+        url = reverse('repository:allRepositories')
+        self.assertEquals(resolve(url).func, allRepositories)
+
+    def test_add_url_is_resolved(self):
+        url = reverse('repository:addRepository')
+        self.assertEquals(resolve(url).func, addRepository)
+
+    def test_delete_url_is_resolved(self):
+        url = reverse('repository:deleteRepository', args=[1])
+        self.assertEquals(resolve(url).func, delete_view)
+
+    def test_update_url_is_resolved(self):
+        url = reverse('repository:updateRepository', args=[1])
+        self.assertEquals(resolve(url).func, update_view)
+
+    def test_detail_url_is_resolved(self):
+        url = reverse('repository:detailRepository', args=[1])
+        self.assertEquals(resolve(url).func, detail_view)
 
 class TestViews(TestCase):
 
@@ -89,5 +136,3 @@ class TestViews(TestCase):
 
         self.assertEquals(response.status_code, 302)
         self.assertEquals(Repository.objects.count(), 1)
-
-    
