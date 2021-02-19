02a89a3feeccfd12dc9c5508a2280162b0219ddd
tjncc <55355686+tjncc@users.noreply.github.com>
2021-02-19 09:16:43 +0100

    Added commands in start.sh file.
    Added makemigrations and test commands before runserver.

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+python github/manage.py makemigrations
 python github/manage.py migrate
-python github/manage.py test user
+python github/manage.py test user repository milestone label
 python github/manage.py runserver 0.0.0.0:8081
