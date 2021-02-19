2070412267f42d05dc68dfa72ebd34f06ae947ab
tjncc <55355686+tjncc@users.noreply.github.com>
2021-02-19 09:04:08 +0100

    Fixed tests for milestone.
    Provided id values for used entities in tests.

--- github/milestone/tests/test_views.py
+++ github/milestone/tests/test_views.py
@@ -5,6 +5,7 @@ from milestone.models import Milestone
 from repository.models import Repository
 from wiki.models import Wiki
 from project.models import Project
+from django.contrib.auth.models import User
 import json
 
 class TestViews(TestCase):
@@ -13,11 +14,14 @@ class TestViews(TestCase):
         self.client = Client()
         self.all_ms = reverse('milestone:allMilestones')
 
-        test_wiki = Wiki.objects.create(content='test content')
-        self.test_repo = Repository.objects.create(name='test repo',wiki_id=test_wiki.id)
-        self.test_project = Project.objects.create(name='test project',description='desc', repository_id=self.test_repo.id)
+        test_wiki = Wiki.objects.create(id=1,content='test content')
+        self.test_repo = Repository.objects.create(id=1,name='test repo',wiki_id=test_wiki.id)
+        self.test_project = Project.objects.create(id=1,name='test project',description='desc', repository_id=1)
 
         self.add_ms = reverse('milestone:addMilestone', args=[self.test_repo.id])
+
+        self.loggedInUser = User.objects.create_user(username='testuser', password='12345')
+        login = self.client.login(username='testuser', password='12345')
 
     def test_add_milestone_POST(self):
         response = self.client.post(self.add_ms, {
@@ -47,12 +51,12 @@ class TestViews(TestCase):
             'dueDate' : '06/25/21'
         })
 
+        test_ms1 = Milestone.objects.get(title = 'UKS')
+
         self.assertEquals(Milestone.objects.count(), 1)
         self.assertEquals(Milestone.objects.get(title = 'UKS').title, 'UKS')
 
-        test_ms1 = Milestone.objects.get(title = 'UKS')
-
-        response = self.client.post(reverse('milestone:updateMilestone', args=[self.test_repo.id, test_ms1.id]), {
+        response = self.client.post(reverse('milestone:updateMilestone', kwargs={'id':self.test_repo.id, 'milestone_id':test_ms1.id}), {
             'title' : 'new title',
             'description' : 'new desc',
             'dueDate' : '06/30/21'
@@ -70,11 +74,11 @@ class TestViews(TestCase):
             'dueDate' : '06/25/21'
         })
 
+        test_ms1 = Milestone.objects.get(title = 'UKS')
+
         self.assertEquals(Milestone.objects.count(), 1)
         self.assertEquals(Milestone.objects.get(title = 'UKS').title, 'UKS')
 
-        test_ms1 = Milestone.objects.get(title = 'UKS')
-
-        response = self.client.post(reverse('milestone:deleteMilestone', args=[self.test_repo.id, test_ms1.id]))
+        response = self.client.post(reverse('milestone:deleteMilestone', kwargs={'id':self.test_repo.id, 'milestone_id':test_ms1.id}))
         self.assertEquals(response.status_code, 302)
         self.assertEquals(Milestone.objects.count(), 0)
